a244af90aa4e032d5a0ccc73ede53371cf36faff
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-04-22 12:52:57 -0700

    Updates

---
+++ precache-manifest.8ff22509dabce094d3c976f4c52c403e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45424ac6d57641e49402833d3d14c75c",
+    "url": "/d3react/static/media/power_outages.45424ac6.csv"
+  },
+  {
+    "revision": "30e1a829c4cd893b61f9",
+    "url": "/d3react/static/js/runtime~main.f87e45fc.js"
+  },
+  {
+    "revision": "555849cc032e0ad76ca8",
+    "url": "/d3react/static/js/main.e56336f2.chunk.js"
+  },
+  {
+    "revision": "b3722979700215e0bccf",
+    "url": "/d3react/static/js/2.cf9575bb.chunk.js"
+  },
+  {
+    "revision": "555849cc032e0ad76ca8",
+    "url": "/d3react/static/css/main.30b5bb97.chunk.css"
+  },
+  {
+    "revision": "b3722979700215e0bccf",
+    "url": "/d3react/static/css/2.005dd1a5.chunk.css"
+  },
+  {
+    "revision": "427a415b6310032befb46515b19226df",
+    "url": "/d3react/index.html"
+  }
+];
