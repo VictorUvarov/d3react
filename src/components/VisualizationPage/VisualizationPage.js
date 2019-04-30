1f68b255c3e94fb4696092b92e045345366635be
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-04-30 01:03:11 -0700

    padding removed from pages, pie chart

--- src/components/VisualizationPage/VisualizationPage.js
+++ src/components/VisualizationPage/VisualizationPage.js
@@ -5,7 +5,6 @@ export default class VisualizationPage extends Component {
   render() {
     return (
       <div className="vis-root">
-        <h1>{this.props.title}</h1>
         <p className="vis-text">{this.props.text}</p>
         {this.props.visualization}
       </div>
