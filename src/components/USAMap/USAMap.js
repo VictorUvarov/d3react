26a6026a65b8523d310943b824f66a44e69d7cb0
VictorUvarov <victoruvarovsacstate@gmail.com>
2019-05-01 22:45:49 -0700

    USA Map topo added and component skeleton made

---
+++ src/components/USAMap/USAMap.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import USAMapTopo from "../../data/USA.json";
+
+export default class USAMap extends Component {
+  componentDidMount() {
+    console.log(USAMapTopo);
+  }
+  render() {
+    return <div />;
+  }
+}
